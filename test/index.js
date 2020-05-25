8ca560f3e749bd35514eafdadd798c5d9efcabd0
Sam Moore <sam@toldmedia.com>
2020-05-24 21:57:05 -0700

    chore: upgrade mocha to 5.2.x; close db connection after tests so mocha exits

--- test/index.js
+++ test/index.js
@@ -19,10 +19,16 @@ function App() {
   return app;
 }
 
-mongoose.connect('mongodb://localhost:27017/mongoose-router-test', { useMongoClient: true });
+const conn = mongoose.connect('mongodb://localhost:27017/mongoose-router-test', {
+  useMongoClient: true
+});
 
 before(function (done) {
   model('__db').db.dropDatabase(done);
+});
+
+after(function (done) {
+  conn.close(done);
 });
 
 describe('module', function () {
